328fe4caef02c5f6bcefe57c576f5a63b466dd0a
Jerrica VanAlstyne <jsnow07@yahoo.com>
2019-12-01 17:47:49 -0500

    removed node modules

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 
 require(path.join(__dirname, './app/routing/htmlRoutues'))(app);
-require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+// require(path.join(__dirname, './app/routing/apiRoutes'))(app);
 // router.get("/")
 // app.get("/", function(req, res) {
 //     res.sendFile(path.join(__dirname, "home.html"));
