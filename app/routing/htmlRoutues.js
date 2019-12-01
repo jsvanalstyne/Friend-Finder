6e7dbec4b8c35f8bddab270948c6b98a695ecd2b
Jerrica VanAlstyne <jsnow07@yahoo.com>
2019-12-01 17:41:06 -0500

    created routes for home and survey

--- app/routing/htmlRoutues.js
+++ app/routing/htmlRoutues.js
@@ -1,17 +1,15 @@
-var express = require("express");
-var path = require("path");
-var app= express();
-var PORT = 3000;
-app.listen(PORT, function(){
-    console.log("Port is listening on "+ PORT);
-});
 
+var path = require("path");
+
+module.exports = function(app){
 app.get("/survey", function(req, response){
-    response.sendFile(path.resolve(__dirname, "../public/survey.html"))
+    response.sendFile(path.join(__dirname, "../public/survey.html"))
+  
 });
-app.get("*", function(req, response){
-    response.sendFile(path.resolve(__dirname, "../public/home.html"))
+app.get("/", function(req, response){
+    response.sendFile(path.join(__dirname, "../public/home.html"))
 });
+}
 
 
 
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 var express = require("express");
 var path = require("path");
 var app= express();
+var bodyParser = require('body-parser');
+var router = express.Router();
 var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
-  });
-  
+
+require(path.join(__dirname, './app/routing/htmlRoutues'))(app);
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+// router.get("/")
+// app.get("/", function(req, res) {
+//     res.sendFile(path.join(__dirname, "home.html"));
+//   });
+app.listen(PORT, function(){
+    console.log("Port is listening on "+ PORT);
+});
