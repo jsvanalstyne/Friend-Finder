ba59f0216829e0ab2f3097db1fd2f17972ee36dc
Jerrica VanAlstyne <jsnow07@yahoo.com>
2019-11-30 20:43:05 -0500

    added path.resolve to direct routing

--- app/routing/htmlRoutues.js
+++ app/routing/htmlRoutues.js
@@ -1,3 +1,17 @@
+var express = require("express");
+var path = require("path");
+var app= express();
+var PORT = 3000;
+app.listen(PORT, function(){
+    console.log("Port is listening on "+ PORT);
+});
+
+app.get("/survey", function(req, response){
+    response.sendFile(path.resolve(__dirname, "../public/survey.html"))
+});
+app.get("*", function(req, response){
+    response.sendFile(path.resolve(__dirname, "../public/home.html"))
+});
 
 
 
