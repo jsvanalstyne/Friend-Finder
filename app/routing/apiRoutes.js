b439f477ea6fc378ad6a610646cb4df4e6bfcce2
Jerrica VanAlstyne <jsnow07@yahoo.com>
2019-12-03 17:49:19 -0500

    updated post request and for loops

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,6 +10,8 @@ module.exports = function (app) {
     app.post("/api/friends", function (req, res) {
         console.log(req.body)
         var newFriend = req.body;
+        var newFriendScores= parseInt(newFriend.scores);
+
         // Compare the newFriend scores to all of the existing friends and return the friend with the best score. 
         var currentMatch;
         var currentScore = 50;
@@ -17,17 +19,17 @@ module.exports = function (app) {
         for (var i = 0; i < friends.length; i++) {
             var difference = 0;
             // Loop over the scores inside the current friend
-            var friendObject = friends[i].score
+            var friendObject = friends[i].score;
             // Get the difference between the two scores (current friend and new friend)
-            for (var j = 0; j < friendObject.length; j++) {
+            for (var j = 0; j < newFriendScores.length; j++) {
                 // increase var difference by difference of friends.
-                difference = Math.abs(friendObject[j] - newFriend.score[j]);
+                difference += Math.abs(friendObject[j] - newFriendScores[j]);
             }
         }
         // Once this loop completes, compare the difference to currentScore
 
         if (difference < currentScore) {
-            currentMatch = friends[i];
+            currentMatch = newFriendScores[i];
             currentScore = difference;
         }
         res.json(currentMatch);
